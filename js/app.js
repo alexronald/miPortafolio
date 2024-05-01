cba5b4b5ad91d8a19d9d7abbe385441fe498b62d
Alex Andrade <alexronald43@gmail.com>
2024-05-01 10:19:23 -0500

    30-04-24

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(function () {
+    $('.hamburger').click(function () {
+        $('.hamburger').toggleClass('open');
+    });
+  });
+
+  $('.carousel .carousel-item').each(function () {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    console.log("hola",next)
+    if (!next.length) {
+        next = $(this).siblings(':first');
+        }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++){
+        console.log($(this))
+        next=next.next();
+        if (!next.length){
+            next=$(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+     }
+    });
